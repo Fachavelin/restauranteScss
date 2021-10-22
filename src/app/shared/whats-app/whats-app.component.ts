7d405c7b510c38c479ca6e783569190c114a5451
Freddy Chavez <freddyalexander2.chavez@gmail.com>
2021-10-22 08:51:54 -0500

    22/10/2021

--- src/app/shared/whats-app/whats-app.component.ts
+++ src/app/shared/whats-app/whats-app.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-whats-app',
   templateUrl: './whats-app.component.html',
-  styleUrls: ['./whats-app.component.scss']
+  styleUrls: ['./whats-app.component.scss'],
 })
 export class WhatsAppComponent implements OnInit {
+  link: string =
+    'https://api.whatsapp.com/send?phone=593982291894&text=Hola%2C+acabo+de+ver+tu+anuncio+estoy+interesado+en+contactarme+contigo';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
